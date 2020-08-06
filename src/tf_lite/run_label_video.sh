4e8185b04774b16be8a625948cd902b48ec392f1
dinesh99981@gmail.com <dinesh99981@gmail.com>
2020-08-06 06:32:57 +0100

    tf lite tested and verified with usb camera

--- src/tf_lite/run_label_video.sh
+++ src/tf_lite/run_label_video.sh
@@ -1,1 +1,1 @@
-python3 scripts/label_video.py --model_file models/mobilenet_v1_1.0_224.tflite --label_file labels/labels.txt --image images/grace_hopper.bmp
+python3 scripts/label_video.py --model models/mobilenet_v1_1.0_224.tflite --labels labels/labels.txt 
--- src/tf_lite/scripts/label_video.py
+++ src/tf_lite/scripts/label_video.py
@@ -1,106 +1,109 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# python3
+#
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-"""label_image for tflite."""
+"""Example using TF Lite to classify objects with the Raspberry Pi camera."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import argparse
+import io
 import time
-
 import numpy as np
-from PIL import Image
-#import tensorflow as tf # TF2
-import tflite_runtime.interpreter as tflite
-
+#import picamera
 import cv2
-
-def load_labels(filename):
-  with open(filename, 'r') as f:
-    return [line.strip() for line in f.readlines()]
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
+def load_labels(path):
+  with open(path, 'r') as f:
+    return {i: line.strip() for i, line in enumerate(f.readlines())}
+
+
+def set_input_tensor(interpreter, image):
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
+
+
+def classify_image(interpreter, image, top_k=1):
+  """Returns a sorted array of classification results."""
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+  output_details = interpreter.get_output_details()[0]
+  output = np.squeeze(interpreter.get_tensor(output_details['index']))
+
+  # If the model is quantized (uint8 data), then dequantize the results
+  if output_details['dtype'] == np.uint8:
+    scale, zero_point = output_details['quantization']
+    output = scale * (output - zero_point)
+
+  ordered = np.argpartition(-output, top_k)
+  return [(i, output[i]) for i in ordered[:top_k]]
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument(
-      '-i',
-      '--image',
-      default='/tmp/grace_hopper.bmp',
-      help='image to be classified')
+      '--model', help='File path of .tflite file.', required=True)
   parser.add_argument(
-      '-m',
-      '--model_file',
-      default='/tmp/mobilenet_v1_1.0_224_quant.tflite',
-      help='.tflite model to be executed')
-  parser.add_argument(
-      '-l',
-      '--label_file',
-      default='/tmp/labels.txt',
-      help='name of file containing labels')
-  parser.add_argument(
-      '--input_mean',
-      default=127.5, type=float,
-      help='input_mean')
-  parser.add_argument(
-      '--input_std',
-      default=127.5, type=float,
-      help='input standard deviation')
-  parser.add_argument(
-      '--num_threads', default=None, type=int, help='number of threads')
+      '--labels', help='File path of labels file.', required=True)
   args = parser.parse_args()
 
-  # Create a interpreter instance with given model
-  interpreter = tflite.Interpreter(
-      model_path=args.model_file)
+  labels = load_labels(args.labels)
+
+  interpreter = Interpreter(args.model)
   interpreter.allocate_tensors()
+  _, height, width, _ = interpreter.get_input_details()[0]['shape']
 
-  # Get the input and output info of given model
-  input_details = interpreter.get_input_details()
-  output_details = interpreter.get_output_details()
+  cap = cv2.VideoCapture(0)
 
-  # check the type of the input tensor
-  floating_model = input_details[0]['dtype'] == np.float32
+  while(True):
+    ret, frame = cap.read()
+    image = cv2.resize(frame, (width, height)) 
+  
+    start_time = time.time()
+    results = classify_image(interpreter, image)
+    elapsed_ms = (time.time() - start_time) * 1000
+    label_id, prob = results[0]
+    print('%s %.2f\n%.1fms' % (labels[label_id], prob,elapsed_ms))
+  
+  cap.release()      
+  '''with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera:
+    camera.start_preview()
+    try:
+      stream = io.BytesIO()
+      for _ in camera.capture_continuous(
+          stream, format='jpeg', use_video_port=True):
+        stream.seek(0)
+        image = Image.open(stream).convert('RGB').resize((width, height),
+                                                         Image.ANTIALIAS)
+        start_time = time.time()
+        results = classify_image(interpreter, image)
+        elapsed_ms = (time.time() - start_time) * 1000
+        label_id, prob = results[0]
+        stream.seek(0)
+        stream.truncate()
+        camera.annotate_text = '%s %.2f\n%.1fms' % (labels[label_id], prob,
+                                                    elapsed_ms)
+    finally:
+      camera.stop_preview()
+'''
 
-  # NxHxWxC, H:1, W:2
-  height = input_details[0]['shape'][1]
-  width = input_details[0]['shape'][2]
-  # Resize given input image to fit the size of input_details
-  img = Image.open(args.image).resize((width, height))
-
-  # add N dim
-  input_data = np.expand_dims(img, axis=0)
-
-  if floating_model:
-    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
-
-  interpreter.set_tensor(input_details[0]['index'], input_data)
-
-  start_time = time.time()
-  interpreter.invoke()
-  stop_time = time.time()
-
-  output_data = interpreter.get_tensor(output_details[0]['index'])
-  results = np.squeeze(output_data)
-
-  top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(args.label_file)
-  for i in top_k:
-    if floating_model:
-      print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
-    else:
-      print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
-
-  print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
+if __name__ == '__main__':
+  main()
